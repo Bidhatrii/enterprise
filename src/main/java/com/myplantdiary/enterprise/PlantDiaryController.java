a01d83f37dc56acc030a610c1060dca5c6288c37
Bidhatri Amatya <abidhatri@gmail.com>
2023-09-21 14:34:55 -0400

    Initial Commit

--- src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
+++ src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -1,7 +1,11 @@
 package com.myplantdiary.enterprise;
 
+import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 
@@ -13,8 +17,48 @@ public class PlantDiaryController{
     */
     @RequestMapping("/")
 
-    public String index(){
-        return"start";
+    public String index(Model model){
+        Specimen specimen = new Specimen();
+        specimen.setDescription("Pawpaw fruit season");
+        specimen.setLatitude("39.74");
+        specimen.setLongitude("-84.51");
+        specimen.setSpecimenId("1003");
+        specimen.setPlantId(84);
+        model.addAttribute(specimen);
+
+        return "start";
     }
 
-        }
+    /**
+     * Endpoint to fetch specimens
+     */
+@GetMapping("/specimen")
+    public ResponseEntity fetchAllSpecimens(){
+    return new ResponseEntity(HttpStatus.OK);
+}
+
+    /**
+     * Endpoint to fetch specimen by ID
+     */
+@GetMapping("/specimen/{id}/")
+    public ResponseEntity fetchSpecimenById(@PathVariable("id") String id){
+    return new ResponseEntity(HttpStatus.OK);
+}
+
+
+    /**
+     * Endpoint to create specimen
+     */
+@PostMapping(value = "/specimen", consumes = "application/json", produces = "application/json")
+    public Specimen createSpecimen(@RequestBody Specimen specimen){
+    return specimen;
+}
+
+    /**
+     * Endpoint to delete specimen by ID
+     */
+@DeleteMapping("/specimen/{id}")
+    public ResponseEntity deleteSpecimen(@PathVariable("id") String id){
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
